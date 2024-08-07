59e63ff4fe25b6cb9efa4e963e88c347d55625f4
Johnnie Massart <johnniemassart@Johnnies-MacBook-Pro.local>
2024-08-07 15:15:22 -0500

    dynamic x, y characters

--- interweaving.py
+++ interweaving.py
@@ -1,6 +1,8 @@
-file_name = input("Enter file name: ")
+# user input file name, number only
+file_name = input("Enter file number (1-5 only): ")
 
 
+# function to read file, store in list
 def read_file(file_name):
     content = []
     with open(f"../files/input{file_name}.txt", "r") as f:
@@ -9,33 +11,48 @@ def read_file(file_name):
     return content
 
 
+# index location of s, x, y values from read_file function
 s = read_file(file_name)[0]
 x = read_file(file_name)[1]
 y = read_file(file_name)[2]
 
 
+# determine if s is long enough
 def valid_length(s, x, y):
     return False if len(x) + len(y) > len(s) else True
 
 
-def ignore_chars(s, x, y):
+# characters present in x, y
+def present_x_y_characters(s, x, y):
+    present_chars = set()
+    entire_string = x + y
+    for char in entire_string:
+        if char not in present_chars:
+            present_chars.add(char)
+    return present_chars
+
+
+# ignore characters not present in x, y at start, end of s
+# return new_s list if no invalid characters in between string s, else false
+def ignore_characters(s, x, y):
+    chars = present_x_y_characters(s, x, y)
     start = 0
     i = 0
     while i < len(s):
-        if s[i] == "0" or s[i] == "1":
+        if s[i] in chars:
             start = i
             break
         i += 1
     end = 0
     j = len(s) - 1
     while j >= 0:
-        if s[j] == "0" or s[j] == "1":
+        if s[j] in chars:
             end = j
             break
         j -= 1
     new_s = []
     for i in range(start, end + 1, 1):
-        if s[i] == "0" or s[i] == "1":
+        if s[i] in chars:
             valid = True
             new_s.append(s[i])
         else:
@@ -44,16 +61,14 @@ def ignore_chars(s, x, y):
     return new_s if valid else valid
 
 
-# print(ignore_chars(s, x, y))
-
-
+# determine if interweaving is possible by utilizing dynamic programming - tabulation
 def canInterweave(s, x, y):
     if valid_length(s, x, y) == False:
         return f"not possible"
-    if ignore_chars(s, x, y) == False:
+    if ignore_characters(s, x, y) == False:
         return f"not possible"
     else:
-        s = ignore_chars(s, x, y)
+        s = ignore_characters(s, x, y)
     table = [False for i in range(len(s) + 1)]
     table[0] = True
     x_table = []
